117af274128abef523c9fc200b8a4544200dd264
ShuaiWang0410 <shuaiwang0410@gmail.com>
2018-10-10 15:49:36 -0400

    Try on AWS

---
+++ source_root_finder.py
@@ -0,0 +1,16 @@
+import os
+path =
+source_roots =
+def find_all_source_roots(path):
+    items = os.listdir(path)
+
+    global source_roots
+    for i in items:
+        absulute_path = os.path.join(path, i)
+        if not os.path.isfile(absulute_path):
+            source_roots += absulute_path + ":"
+            find_all_source_roots(absulute_path)
+
+find_all_source_roots(source_roots)
+
+
