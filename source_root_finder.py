aa55f3590b4cc2d22b8206ab2bdd1a93962b70b5
ShuaiWang0410 <shuaiwang0410@gmail.com>
2018-10-10 16:10:05 -0400

    Try on AWS

--- source_root_finder.py
+++ source_root_finder.py
@@ -1,18 +1,21 @@
 import os
 source_roots = "/home/ubuntu/ShuaiWang/sw-face-net/facenet"
-ignores = ["data"]
+paths = ":"
+#source_roots = "/Users/gm-vv-ang/Documents/UBCSE/courses/2018 FALL/CSE 676/Projects/facenet/"
+ignores = ["data", ".idea", ".git", ".project", ".pydevproject", ".DS_Store", ".pylintrc"]
 def find_all_source_roots(path):
     items = os.listdir(path)
 
-    global source_roots
+    global paths
     for i in items:
         if i in ignores:
             continue
         absulute_path = os.path.join(path, i)
         if not os.path.isfile(absulute_path):
-            source_roots += absulute_path + ":"
+            paths += absulute_path + ":"
             find_all_source_roots(absulute_path)
-    print(source_roots[:-1])
+
 find_all_source_roots(source_roots)
+print(paths[:-1])
 
 
